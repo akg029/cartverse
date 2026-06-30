d4c563f04b7bce7d2958413d30b95f6f48549b6a
akg029 <akg2998@gmail.com>
2026-06-30 14:47:18 +0530

    fix: project structure re-designed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../styles/reset.css";
-import "../styles/variables.css";
-import "../styles/global.css";
+import "../src/styles/reset.css";
+import "../src/styles/variables.css";
+import "../src/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
